b3f631f584270c892fbc10b4483d4e74980a0a25
Nick <parikmaher534@gmail.com>
2012-09-10 17:12:15 +0300

    Update base structure

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,20 @@ Heroes.config = {
     "bg2": "images/bg/tgrs022.png",
     "bg3": "images/bg/tgrs023.png",
     "bg4": "images/bg/tgrs021.png",
-    "bg5": "images/bg/tgrs030.png"
+    "bg5": "images/bg/tgrs020.png",
+    "bg6": "images/bg/tgrs030.png",
+    "bg7": "images/bg/tgrs010.png",
+    "bg8": "images/bg/tgrb000.png",
+    "bg9": "images/bg/tgrb001.png",
+    "bg10": "images/bg/tgrb002.png",
+    "bg11": "images/bg/tgrb003.png",
+    "bg12": "images/bg/tgrb004.png",
+    "bg13": "images/bg/tgrs011-r.png",
+    "bg14": "images/bg/tgrs001-r.png",
+    "bg15": "images/bg/tgrs020-b.png",
+    "bg16": "images/bg/tgrs021-b.png",
+    "bg17": "images/bg/tgrs001-r-b.png",
+    "bg18": "images/bg/tgrs001-l-b.png"
   }
 };
 
